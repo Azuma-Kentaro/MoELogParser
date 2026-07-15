280c24e586251a6becb2ce34b022598b35023d77
kentaro Azuma <azuma.199x@gmail.com>
2026-07-15 18:17:29 +0900

    クラスを解体して関数だけ抽出

--- bank_data.py
+++ bank_data.py
@@ -16,59 +16,49 @@ class BankDataEntry:
     finished: str
     plus: str
 
-class BankEntryManager:
-    def __init__(self):
-        self._entries = []
+def update_entry(entry_list, new_entry):
+    """
+    所持枠拡張クエストのクリア状況に関するエントリを更新する
+    同じ所持枠拡張クエストに関するエントリが重複した場合には最新のエントリで置き換える
+    既存エントリを更新した場合はTrueを返し、そうでない場合はFalseを返す
+    """
+    updated = False
+    for i, existing_entry in enumerate(entry_list):
+        # クエスト名が違うので次のエントリに対する試行を開始する
+        if existing_entry.quest_name != new_entry.quest_name:
+            continue
+        # 既存エントリの方が日付が新しい場合は最新のエントリで置き換える
+        if existing_entry <= new_entry:
+            logger.debug(f"update {existing_entry.quest_name} {existing_entry.finished} {existing_entry.log_date} {existing_entry.log_time} -> {new_entry.finished} {new_entry.log_date} {new_entry.log_time}")
+            entry_list[i] = new_entry
+            updated = True
+        # 同じクエスト名のエントリは重複しないので、置き換えたか否かに関係なくループを終了する
+        break
+    else:
+        # break以外でループが終わった場合は、同じクエスト名のエントリが存在しなかった場合なので
+        # 新しいエントリとして、リストに追加する
+        logger.debug(f"new {new_entry.quest_name} {new_entry.finished} {new_entry.log_date} {new_entry.log_time}")
+        entry_list.append(new_entry)
+    return updated
 
-    @property
-    def entries(self):
-        return self._entries
 
-    def update_entry(self, new_entry):
-        """
-        所持枠拡張クエストのクリア状況に関するエントリを更新する
-        同じ所持枠拡張クエストに関するエントリが重複した場合には最新のエントリで置き換える
-        既存エントリを更新した場合はTrueを返し、そうでない場合はFalseを返す
-        """
-        updated = False
-        for i, existing_entry in enumerate(self._entries):
-            # クエスト名が違うので次のエントリに対する試行を開始する
-            if existing_entry.quest_name != new_entry.quest_name:
-                continue
-            # 既存エントリの方が日付が新しい場合は最新のエントリで置き換える
-            if existing_entry <= new_entry:
-                logger.debug(f"update {existing_entry.quest_name} {existing_entry.finished} {existing_entry.log_date} {existing_entry.log_time} -> {new_entry.finished} {new_entry.log_date} {new_entry.log_time}")
-                self._entries[i] = new_entry
-                updated = True
-            # 同じクエスト名のエントリは重複しないので、置き換えたか否かに関係なくループを終了する
-            break
-        else:
-            # break以外でループが終わった場合は、同じクエスト名のエントリが存在しなかった場合なので
-            # 新しいエントリとして、リストに追加する
-            logger.debug(f"new {new_entry.quest_name} {new_entry.finished} {new_entry.log_date} {new_entry.log_time}")
-            self._entries.append(new_entry)
-        return updated
+def convert_log_to_entries(log_list):
+    """
+    ログファイルのテキストに対して所持枠拡張クエストの情報をエントリとして抽出する
+    """
+    parser = BankParser()
+    # 作業過程で抽出したデータの管理はエントリマネージャに任せる
+    # 最終的な結果だけが必要なので、関数終了時に破棄されるようにローカル変数としている
+    entry_list = []
+    for log in log_list:
+        # 所持枠拡張クエストのエントリとして抽出を試みる
+        new_entry = parser.create_entry(log)
+        if new_entry:
+            # 新規エントリなら追加、重複なら最新のエントリだけ残す
+            update_entry(entry_list, new_entry)
 
-class BankLogConverter:
-    def __init__(self, parser):
-        self._parser = parser
-
-    def convert(self, log_list):
-        """
-        ログファイルのテキストに対して所持枠拡張クエストの情報をエントリとして抽出する
-        """
-        # 作業過程で抽出したデータの管理はエントリマネージャに任せる
-        # 最終的な結果だけが必要なので、関数終了時に破棄されるようにローカル変数としている
-        entry_manager = BankEntryManager()
-        for log in log_list:
-            # 所持枠拡張クエストのエントリとして抽出を試みる
-            entry = self._parser.create_entry(log)
-            if entry:
-                # 新規エントリなら追加、重複なら最新のエントリだけ残す
-                entry_manager.update_entry(entry)
-
-        # 最終的なエントリ情報を返して処理を終了する
-        return entry_manager.entries
+    # 最終的なエントリ情報を返して処理を終了する
+    return entry_list
 
 class BankParser:
     def __init__(self):
@@ -97,10 +87,9 @@ class BankParser:
         return BankDataEntry(log_date, log_time, quest_name, finished, plus)
 
 def get_bank_data(filename):
-    converter = BankLogConverter(BankParser())
     # ゲームの仕様で、生成するファイルのエンコーディングはShift JIS
     with open(filename, "r", encoding="shift-jis") as logfile:
-        bank_entries = converter.convert(logfile)
+        bank_entries = convert_log_to_entries(logfile)
     return bank_entries
 
 if __name__ == "__main__":
