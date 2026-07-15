4bb56f67e7996c977420be0753f142772d35430b
kentaro Azuma <azuma.199x@gmail.com>
2026-07-15 17:29:02 +0900

    クラスにする理由を感じなかったのでクラスを削除

--- character_manager.py
+++ character_manager.py
@@ -17,63 +17,51 @@ MOE_USERDATA_DEFAULT_PATH = r"userdata"
 
 CharacterDirectory  = namedtuple("CharacterDirectory", ["filepath", "server", "name"])
 
-class MoECharacterManager:
-    def __init__(self, directory_path, userdata_path):
-        self._directory_path = directory_path
-        self._userdata_path = userdata_path
+def normalize_name(name):
+    """
+    フォルダ名に含まれる.を除去してキャラクター名に変換する
+    ゲーム側の仕様で、英大文字が含まれるキャラクター名の場合は
+    ディレクトリ名において英大文字の直後にドットがついている
 
-    @staticmethod
-    def normalize_name(name):
-        """
-        フォルダ名に含まれる.を除去してキャラクター名に変換する
-        ゲーム側の仕様で、英大文字が含まれるキャラクター名の場合は
-        ディレクトリ名において英大文字の直後にドットがついている
+    >>> normalize_name("T.est")
+    'Test'
+    """
+    normalized = name.replace('.', '')
+    return normalized
 
-        >>> MoECharacterManager.normalize_name("T.est")
-        'Test'
-        """
-        normalized = name.replace('.', '')
-        return normalized
+def extract_character_server_and_name(directory_name):
+    """
+    キャラクタ毎のディレクトリ名からサーバ名とキャラクター名を抽出する
 
-    @staticmethod
-    def extract_character_server_and_name(directory_name):
-        """
-        キャラクタ毎のディレクトリ名からサーバ名とキャラクター名を抽出する
-
-        >>> MoECharacterManager.extract_character_server_and_name("DIAMOND_T.est_")
-        ('DIAMOND', 'Test')
-        """
-        m = re.match(r"^(?P<server>(DIAMOND)|(EMERALD)|(PEARL))_(?P<name>.+)_", directory_name)
-        if not m:
-            return (None, None)
-        server = m.group("server")
-        name = MoECharacterManager.normalize_name(m.group("name"))
-        return (server, name)
-
-    def get_character_list(self):
-        cl = []
-        try:
-            p = Path(self._directory_path, self._userdata_path)
-
-            for subdir in p.iterdir():
-                if not subdir.is_dir():
-                    continue
-                (server, name) = MoECharacterManager.extract_character_server_and_name(subdir.name)
-                if server and name:
-                    cl.append(CharacterDirectory(subdir, server, name))
-
-        except Exception as e:
-            # エラーは記録するが、処理を継続するために意図的に握りつぶす
-            logger.error(e)
-        finally:
-            pass
-
-        return cl
+    >>> extract_character_server_and_name("DIAMOND_P.layerN.ame_")
+    ('DIAMOND', 'PlayerName')
+    """
+    m = re.match(r"^(?P<server>(DIAMOND)|(EMERALD)|(PEARL))_(?P<name>.+)_", directory_name)
+    if not m:
+        return (None, None)
+    server = m.group("server")
+    name = normalize_name(m.group("name"))
+    return (server, name)
 
 def get_character_list(directory_path=MOE_DIRECTORY_DEFAULT_PATH, userdata_path=MOE_USERDATA_DEFAULT_PATH):
-    m = MoECharacterManager(directory_path, userdata_path)
-    character_list = m.get_character_list()
-    return character_list
+    cl = []
+    try:
+        p = Path(directory_path, userdata_path)
+
+        for subdir in p.iterdir():
+            if not subdir.is_dir():
+                continue
+            (server, name) = extract_character_server_and_name(subdir.name)
+            if server and name:
+                cl.append(CharacterDirectory(subdir, server, name))
+
+    except Exception as e:
+        # エラーは記録するが、処理を継続するために意図的に握りつぶす
+        logger.error(e)
+    finally:
+        pass
+
+    return cl
 
 if __name__ == "__main__":
     import doctest
