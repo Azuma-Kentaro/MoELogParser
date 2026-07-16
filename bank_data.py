e400d9970f2085ced95c14e24b1e8010e8e10045
kentaro Azuma <azuma.199x@gmail.com>
2026-07-16 11:59:24 +0900

    クラスを削除してメソッドを抽出

--- bank_data.py
+++ bank_data.py
@@ -41,50 +41,46 @@ def update_entry(entry_list, new_entry):
         entry_list.append(new_entry)
     return updated
 
+# 複数の行に対して同じ正規表現を適用するので、コンパイル済みのパターンを用いて効率化する。
+# 日付部分は所持枠拡張以外にも共通のパターンなので、他の処理にも流用できる。
+date_pattern = r"^(?P<yymmdd>[0-9]{2}/[0-9]{2}/[0-9]{2}) (?P<hhmmss>[0-9]{2}:[0-9]{2}:[0-9]{2})"
+# 所持枠拡張クエストのパターン
+quest_pattern = r": \[ (?P<finish>.) \] (?P<quest_name>.+) : \+ (?P<plus>[0-9])$"
+# どちらも同じ行に表示されるので、1つの行に対して処理するために結合する。
+bank_pattern = re.compile(''.join((date_pattern, quest_pattern)))
+
+def create_entry(line):
+    """
+    与えられた行データが所持枠拡張クエストに関するものであれば、データ化したエントリ情報を返す
+    """
+    m = bank_pattern.match(line)
+    if m is None:
+        return None
+
+    log_date = datetime.date.fromisoformat("20" + m.group("yymmdd").replace('/', '-'))
+    log_time = datetime.time.fromisoformat(m.group("hhmmss"))
+    quest_name = m.group("quest_name")
+    finished = m.group("finish")
+    plus = m.group("plus")
+
+    return BankDataEntry(log_date, log_time, quest_name, finished, plus)
 
 def convert_log_to_entries(log_list):
     """
     ログファイルのテキストに対して所持枠拡張クエストの情報をエントリとして抽出する
     """
-    parser = BankParser()
     # 作業過程で抽出したデータの管理はエントリマネージャに任せる
     # 最終的な結果だけが必要なので、関数終了時に破棄されるようにローカル変数としている
     entry_list = []
     for log in log_list:
         # 所持枠拡張クエストのエントリとして抽出を試みる
-        new_entry = parser.create_entry(log)
+        new_entry = create_entry(log)
         if new_entry:
             # 新規エントリなら追加、重複なら最新のエントリだけ残す
             update_entry(entry_list, new_entry)
 
     # 最終的なエントリ情報を返して処理を終了する
     return entry_list
-
-class BankParser:
-    def __init__(self):
-        # 複数の行に対して同じ正規表現を適用するので、コンパイル済みのパターンを用いて効率化する。
-        # 日付部分は所持枠拡張以外にも共通のパターンなので、他の処理にも流用できる。
-        date_pattern = r"^(?P<yymmdd>[0-9]{2}/[0-9]{2}/[0-9]{2}) (?P<hhmmss>[0-9]{2}:[0-9]{2}:[0-9]{2})"
-        # 所持枠拡張クエストのパターン
-        quest_pattern = r": \[ (?P<finish>.) \] (?P<quest_name>.+) : \+ (?P<plus>[0-9])$"
-        # どちらも同じ行に表示されるので、1つの行に対して処理するために結合する。
-        self._pattern = re.compile(''.join((date_pattern, quest_pattern)))
-
-    def create_entry(self, line):
-        """
-        与えられた行データが所持枠拡張クエストに関するものであれば、データ化したエントリ情報を返す
-        """
-        m = self._pattern.match(line)
-        if m is None:
-            return None
-
-        log_date = datetime.date.fromisoformat("20" + m.group("yymmdd").replace('/', '-'))
-        log_time = datetime.time.fromisoformat(m.group("hhmmss"))
-        quest_name = m.group("quest_name")
-        finished = m.group("finish")
-        plus = m.group("plus")
-
-        return BankDataEntry(log_date, log_time, quest_name, finished, plus)
 
 def get_bank_data(filename):
     # ゲームの仕様で、生成するファイルのエンコーディングはShift JIS
